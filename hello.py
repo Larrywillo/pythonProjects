b7de7fe842630d9afd51110e7f43a072bba133bc
larrywillo <owolanre1@gmail.com>
2024-11-19 09:47:29 -0500

    Modified file to helloworld v2

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def sayHello(userName):
+    print("Hello", userName)
+
+def getUserInput():
+    userName = input("Enter your name: ")
+    return userName
+
+
+myName = getUserInput()
+sayHello(myName)
